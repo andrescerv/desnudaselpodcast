85ea2cc806358a5732ead33b100e7ef4881ebd3c
andrescerv <andres.cerv.7@gmail.com>
2019-04-06 21:12:21 -0600

    spotify play button added

--- client/src/Components/Desnudas/Desnudas.js
+++ client/src/Components/Desnudas/Desnudas.js
@@ -5,8 +5,9 @@ class Desnudas extends Component {
     state = {  }
     render() { 
         return (
-            <div>
-                <h1>Desnudas</h1>
+            <div className="contenido">
+                {/* <h1>Desnudas</h1> */}
+                <iframe title="spotify-play-button" src="https://open.spotify.com/embed/show/6E8XEbQ0abhCQsTbFD2t5i" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
             </div>
         )
     }
