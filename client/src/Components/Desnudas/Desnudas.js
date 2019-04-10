dcaef04ca74949a8766156618e27ea643869cea7
andrescerv <andres.cerv.7@gmail.com>
2019-04-09 20:16:49 -0500

    home img added

--- client/src/Components/Desnudas/Desnudas.js
+++ client/src/Components/Desnudas/Desnudas.js
@@ -1,15 +1,16 @@
 import React, {Component} from 'react';
 import './Desnudas.css'
+import homeLogo from "../../material_AV/equipoDesnudas.jpg"
 
 class Desnudas extends Component {
     state = {  }
     render() { 
         return (
-            <div class='wrapper'>
-                <div class="homeGIF">
-                    <img src="/home/andres/Documents/devlife/BEDU/desnudaselpodcast/material_AV/equipoDesnudas.jpeg" alt="El equipo de las desnudas"/>
+            <div className='wrapper'>
+                <div className="homeGIF">
+                    <img src={homeLogo} alt="El equipo de las desnudas"/>
                 </div>
-                <div class="spotify">
+                <div className="spotify">
                     <iframe title="spotify-play-button" src="https://open.spotify.com/embed/show/6E8XEbQ0abhCQsTbFD2t5i" bg-color="green" width="80%" height="380" frameborder="0" allowtransparency="false" allow="encrypted-media"></iframe>
                 </div>
             </div>
