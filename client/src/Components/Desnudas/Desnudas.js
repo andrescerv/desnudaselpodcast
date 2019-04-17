1cc67cfa31a2d2e8d8443f2c498c318bc6c0cd14
andrescerv <andres.cerv.7@gmail.com>
2019-04-16 21:28:56 -0500

    trying to import props

--- client/src/Components/Desnudas/Desnudas.js
+++ client/src/Components/Desnudas/Desnudas.js
@@ -1,7 +1,11 @@
 import React, {Component} from 'react';
 import './Desnudas.css'
 import homeLogo from "../../material_AV/equipoDesnudas.jpg"
-import {PlaylistSoundPlayer} from '../Soundcloud/Soundcloud'
+import BasicSCWidget from '../Soundcloud/BasicSCWidget';
+
+const resolveUrl = 'https://soundcloud.com/desnudas/cortorreo-4-alejandra-cartagena-el-metoo-y-nuestros-derechos'
+const clientId = 'xIa292zocJP1G1huxplgJKVnK0V3Ni9D'
+
 
 class Desnudas extends Component {
     state = {  }
@@ -11,10 +15,12 @@ class Desnudas extends Component {
                 <div className="homeGIF">
                     <img src={homeLogo} alt="El equipo de las desnudas"/>
                 </div>
+                <BasicSCWidget 
+                    resolveUrl={resolveUrl}
+                    clientId={clientId}/>
                 <div className="spotify">
                     <iframe title="spotify-play-button" src="https://open.spotify.com/embed/show/6E8XEbQ0abhCQsTbFD2t5i" bg-color="green" width="80%" height="380" frameborder="0" allowtransparency="false" allow="encrypted-media"></iframe>
                 </div>
-                <PlaylistSoundPlayer/>
             </div>
         )
     }
---
+++ client/src/Components/Soundcloud/BasicSCWidget.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+// import PropTypes from 'prop-types';
+import { withSoundCloudAudio } from 'react-soundplayer/addons';
+import { PlayButton, Progress, VolumeControl, NextButton, Cover, Icons } from 'react-soundplayer/components';
+
+const { SoundCloudLogoSVG } = Icons;
+
+class ProgressSoundPlayer extends Component {
+
+  render() {
+    const { track, currentTime, duration, resolveUrl, clientId } = this.props;
+    console.log(this.props);
+    console.log(track);
+    return (
+      <div className="p2 border navy mt1 mb3 flex flex-center rounded">
+        <PlayButton className="flex-none h4 mr2 button white btn-big button-outline button-grow bg-orange circle" {...this.props} />
+        <div className="flex-auto">
+          <h2 className="h4 nowrap m0">{track ? track.user.username : ''}</h2>
+          <h2 className="h4 nowrap caps m0">{track ? track.title : ''}</h2>
+          <div className='flex flex-center'>
+            <VolumeControl
+              className='mr2 flex flex-center'
+              buttonClassName="flex-none h6 button white btn-small button-outline button-grow bg-orange circle btn-square"
+              {...this.props} />
+            <NextButton
+              />
+            <Cover
+              className={String}
+              trackName={String}
+              artistName={String}
+              backgroundUrl={this.props.track} />
+            <Progress
+              className="mt1 mb1 rounded"
+              innerClassName="rounded-left"
+              value={(currentTime / duration) * 100 || 0}
+              {...this.props} />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+// ProgressSoundPlayer.propTypes = {
+//   resolveUrl: this.props.resolveUrl,
+//   clientId: this.props.clientId
+// };
+
+export default withSoundCloudAudio(ProgressSoundPlayer);
